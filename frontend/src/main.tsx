a8b300c2b8b2c01abff4ed3e125fe4d6c93f2796
Youngmin Joo <ymjoo827@gmail.com>
2025-01-08 16:12:02 +0900

    Set custom themes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import system from "./themes/theme";
+import "./themes/fonts.css";
 
 createRoot(document.getElementById("root")!).render(
-  <ChakraProvider value={defaultSystem}>
+  <ChakraProvider value={system}>
     <RouterProvider router={router} />
   </ChakraProvider>
 );
