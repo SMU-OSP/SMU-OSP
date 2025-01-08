5bfb27224e333497a26d15458ac63545b7737698
Youngmin Joo <ymjoo827@gmail.com>
2025-01-08 14:28:50 +0900

    Setup router

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Stack>
+      <h1>Header</h1>
+    </Stack>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider value={defaultSystem}>
+    <RouterProvider router={router} />
+  </ChakraProvider>
+);
