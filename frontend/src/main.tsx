34d4083bdc3772ca0c4cbf9e6283e1ef4d8ca656
Youngmin Joo <ymjoo827@gmail.com>
2025-01-09 14:31:40 +0900

    Set react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import system from "./themes/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <ChakraProvider value={system}>
-    <RouterProvider router={router} />
-  </ChakraProvider>
+  <QueryClientProvider client={client}>
+    <ChakraProvider value={system}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </QueryClientProvider>
 );
